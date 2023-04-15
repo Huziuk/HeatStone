ffe35f5059935613803a352cea369b91634300b1
Yevhen Huziuk <yevhen.huziuk@neustars.team>
2023-04-15 19:57:27 +0300

    Develop header layout

---
+++ Pages/About/About.razor.cs
@@ -0,0 +1,10 @@
+﻿using HeatStone.Constants;
+using Microsoft.AspNetCore.Components;
+
+namespace HeatStone.Pages.About;
+
+[Route(Routes.About)]
+public partial class About
+{
+    
+}
---
+++ Pages/Catalog/Catalog.razor.cs
@@ -0,0 +1,10 @@
+﻿using HeatStone.Constants;
+using Microsoft.AspNetCore.Components;
+
+namespace HeatStone.Pages.Catalog;
+
+[Route(Routes.Catalog)]
+public partial class Catalog
+{
+    
+}
